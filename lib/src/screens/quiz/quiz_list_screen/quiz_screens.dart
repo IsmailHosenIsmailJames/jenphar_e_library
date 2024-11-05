21ae8c7352a36847ba6bad61af9012ac53d8fff5
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-11-05 18:52:16 +0600

    bug fix

--- lib/src/screens/quiz/quiz_list_screen/quiz_screens.dart
+++ lib/src/screens/quiz/quiz_list_screen/quiz_screens.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 
@@ -59,7 +60,7 @@ class _QuizScreensState extends State<QuizScreens> {
       }
     } else {
       showToastNotification(
-        msg: 'Something went worng',
+        msg: 'Something went wrong',
         context: context,
         type: ToastificationType.error,
       );
@@ -177,13 +178,19 @@ class _QuizScreensState extends State<QuizScreens> {
                 child: Text("There is no Quiz available"),
               );
             } else {
-              return ListView.builder(
-                itemCount: controller.quizList.length,
-                padding: const EdgeInsets.all(25),
-                itemBuilder: (context, index) {
-                  return quizCard(controller.quizList[index]);
-                },
-              );
+              return StreamBuilder(
+                  stream: Stream.periodic(
+                    Duration(seconds: 1),
+                  ),
+                  builder: (context, event) {
+                    return ListView.builder(
+                      itemCount: controller.quizList.length,
+                      padding: const EdgeInsets.all(25),
+                      itemBuilder: (context, index) {
+                        return quizCard(controller.quizList[index]);
+                      },
+                    );
+                  });
             }
           },
         ),
@@ -192,163 +199,179 @@ class _QuizScreensState extends State<QuizScreens> {
   }
 
   Widget quizCard(QuizListModel quizListModel) {
-    final startDate = quizListModel.startTime;
-    final endDate = quizListModel.endTime;
-    final now = DateTime.now();
-    int status = 0;
-    if (now.compareTo(startDate) == 1 && now.compareTo(endDate) == -1) {
-      status = 0;
-    } else if (now.compareTo(startDate) == -1) {
-      status = 1;
-    } else {
-      status = -1;
-    }
-    log(status.toString());
-    return Container(
-      margin: const EdgeInsets.only(top: 10, bottom: 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 10,
-            color: Colors.grey.shade500,
-          )
-        ],
-      ),
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(
-                  10,
-                ),
-                topRight: Radius.circular(10),
-              ),
-              color: Color.fromARGB(255, 53, 121, 177),
+    DateTime startDate = quizListModel.startTime;
+    DateTime endDate = quizListModel.endTime;
+    DateTime now = DateTime.now();
+
+    startDate = DateTime(startDate.year, startDate.month, startDate.day,
+        startDate.hour, startDate.minute, startDate.second);
+    endDate = DateTime(endDate.year, endDate.month, endDate.day, endDate.hour,
+        endDate.minute, endDate.second);
+    now = DateTime(
+        now.year, now.month, now.day, now.hour, now.minute, now.second);
+
+    int status = checkTimeStatus(startDate, endDate);
+
+    return quizListModel.status == "Unpublished"
+        ? SizedBox()
+        : Container(
+            margin: const EdgeInsets.only(top: 10, bottom: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 10,
+                  color: Colors.grey.shade500,
+                )
+              ],
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  const Gap(5),
-                  Text(
-                    "Topic",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey.shade200,
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(
+                        10,
+                      ),
+                      topRight: Radius.circular(10),
+                    ),
+                    color: Color.fromARGB(255, 53, 121, 177),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        const Gap(5),
+                        Text(
+                          "Topic",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey.shade200,
+                          ),
+                        ),
+                        Text(
+                          quizListModel.name,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Text(
-                    quizListModel.name,
-                    style: const TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                ),
+                const Gap(10),
+                Text(
+                  "Exam Duration",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade700,
+                  ),
+                ),
+                Text(
+                  "${quizListModel.timeDuration} minutes",
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Gap(10),
+                Text(
+                  "Exam Start",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade700,
+                  ),
+                ),
+                Text(
+                  "Date: ${quizListModel.startTime.toIso8601String().split('T')[0]}, Time: ${quizListModel.startTime.toIso8601String().split('T')[1].split('.')[0]}",
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Gap(10),
+                Text(
+                  "Exam End",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade700,
+                  ),
+                ),
+                Text(
+                  "Date: ${quizListModel.endTime.toIso8601String().split('T')[0]}, Time: ${quizListModel.endTime.toIso8601String().split('T')[1].split('.')[0]}",
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    final infoBox = Hive.box('info');
+                    final userInfo =
+                        infoBox.get('userInfo', defaultValue: null);
+                    final workAreaT = userInfo['work_area_t'];
+                    final quizListID = quizListModel.id;
+                    if (status == 0) {
+                      await Get.to(
+                        () => QuestionsScreen(
+                          endtime: quizListModel.endTime,
+                          startDate: quizListModel.startTime,
+                          examDuration: quizListModel.timeDuration,
+                          id: quizListID,
+                          titleOfTopice: widget.title,
+                          workAreaT: workAreaT,
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                      color: status == 0
+                          ? const Color.fromARGB(255, 30, 59, 102)
+                          : Colors.grey,
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      status == 0
+                          ? 'EXAM IS RUNNING'
+                          : status == 1
+                              ? "EXAM IS UPCOMING"
+                              : "TIME OVER",
+                      style: const TextStyle(
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          const Gap(10),
-          Text(
-            "Exam Duration",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey.shade700,
-            ),
-          ),
-          Text(
-            "${quizListModel.timeDuration} minutes",
-            style: const TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Gap(10),
-          Text(
-            "Exam Start",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey.shade700,
-            ),
-          ),
-          Text(
-            "Date: ${quizListModel.startTime.toIso8601String().split('T')[0]}, Time: ${quizListModel.startTime.toIso8601String().split('T')[1].split('.')[0]}",
-            style: const TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Gap(10),
-          Text(
-            "Exam End",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey.shade700,
-            ),
-          ),
-          Text(
-            "Date: ${quizListModel.endTime.toIso8601String().split('T')[0]}, Time: ${quizListModel.endTime.toIso8601String().split('T')[1].split('.')[0]}",
-            style: const TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          GestureDetector(
-            onTap: () async {
-              final infoBox = Hive.box('info');
-              final userInfo = infoBox.get('userInfo', defaultValue: null);
-              final workAreaT = userInfo['work_area_t'];
-              final quizListID = quizListModel.id;
-              if (status == 0) {
-                await Get.to(
-                  () => QuestionsScreen(
-                    endtime: quizListModel.endTime,
-                    startDate: quizListModel.startTime,
-                    examDuration: quizListModel.timeDuration,
-                    id: quizListID,
-                    titleOfTopice: widget.title,
-                    workAreaT: workAreaT,
-                  ),
-                );
-              }
-            },
-            child: Container(
-              height: 60,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
                 ),
-                color: status == 1
-                    ? const Color.fromARGB(255, 30, 59, 102)
-                    : Colors.grey,
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                status == 0
-                    ? 'EXAM IS RUNNING'
-                    : status == 1
-                        ? "EXAM IS UPCOMMING"
-                        : "TIME OVER",
-                style: const TextStyle(
-                  fontSize: 21,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+          );
+  }
+}
+
+int checkTimeStatus(DateTime startDate, DateTime endDate) {
+  final now = DateTime.now();
+
+  if (now.isAfter(endDate)) {
+    return -1; // Time over
+  } else if (now.isBefore(startDate)) {
+    return 1; // Time upcoming
+  } else {
+    return 0; // Running
   }
 }
